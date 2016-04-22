8c510ba98e1bf904be889bea7be5cf0904a4d36a
Andres Garcia <afgarcia86@gmail.com>
2016-04-22 13:24:36 -0400

    wip

--- arnold_bot.js
+++ arnold_bot.js
@@ -1,9 +1,9 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+         ______     ______     ______   __  __     __     ______
+        /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+        \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+         \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+          \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
 
 
 This is a sample Slack bot built with Botkit.
@@ -15,232 +15,300 @@ This bot demonstrates many of the core features of Botkit:
 * Reply to messages
 * Use the conversation system to ask questions
 * Use the built in storage system to store and retrieve information
-  for a user.
+for a user.
 
 # RUN THE BOT:
 
-  Get a Bot token from Slack:
+Get a Bot token from Slack:
 
-    -> http://my.slack.com/services/new/bot
+  -> http://my.slack.com/services/new/bot
 
-  Run your bot from the command line:
+Run your bot from the command line:
 
-    token=<MY TOKEN> node slack_bot.js
+  token=<MY TOKEN> node slack_bot.js
 
 # USE THE BOT:
 
-  Find your bot inside Slack to send it a direct message.
+Find your bot inside Slack to send it a direct message.
 
-  Say: "Hello"
+Say: "Hello"
 
-  The bot will reply "Hello!"
+The bot will reply "Hello!"
 
-  Say: "who are you?"
+Say: "who are you?"
 
-  The bot will tell you its name, where it running, and for how long.
+The bot will tell you its name, where it running, and for how long.
 
-  Say: "Call me <nickname>"
+Say: "Call me <nickname>"
 
-  Tell the bot your nickname. Now you are friends.
+Tell the bot your nickname. Now you are friends.
 
-  Say: "who am I?"
+Say: "who am I?"
 
-  The bot will tell you your nickname, if it knows one for you.
+The bot will tell you your nickname, if it knows one for you.
 
-  Say: "shutdown"
+Say: "shutdown"
 
-  The bot will ask if you are sure, and then shut itself down.
+The bot will ask if you are sure, and then shut itself down.
 
-  Make sure to invite your bot into other channels using /invite @<my bot>!
+Make sure to invite your bot into other channels using /invite @<my bot>!
 
 # EXTEND THE BOT:
 
-  Botkit has many features for building cool and useful bots!
+Botkit has many features for building cool and useful bots!
 
-  Read all about it here:
+Read all about it here:
 
-    -> http://howdy.ai/botkit
+  -> http://howdy.ai/botkit
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
 if (!process.env.token) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
+  console.log('Error: Specify token in environment');
+  process.exit(1);
 }
 
+var request = require('superagent');
 var Botkit = require('botkit');
 var os = require('os');
 
 var controller = Botkit.slackbot({
-    debug: true,
+  debug: true,
 });
 
 var bot = controller.spawn({
-    token: process.env.token
+  token: process.env.token
 }).startRTM();
 
+controller.hears(['hello', 'hi', 'hey', 'welcome', 'yo', 'lets go', 'workout'], 'direct_message,direct_mention,ambient', function(bot, message) {
 
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+  sayHello = function(response, convo) {
+    var intro = pickIntro();
+    convo.say(intro);
+    askHowMany(response, convo);
+    convo.next();
+  }
 
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
+  askHowMany = function(response, convo) {
+    convo.ask('How many workouts do you want to do today?', [
+      {
+        pattern:  /^\d{1,2}$/,
+        callback: function(response, convo) {
 
+          if(response.text > 2){
 
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
-});
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
+            bot.api.reactions.add({
+              timestamp: response.ts,
+              channel: response.channel,
+              name: pickGoodEmoji(),
+            }, function(err, res) {
+              if (err) {
+                  bot.botkit.log('Failed to add emoji reaction :(', err);
+              }
             });
+
+            workout(response, convo);
+            convo.next();
+
+          } else {
+            bot.api.reactions.add({
+              timestamp: response.ts,
+              channel: response.channel,
+              name: pickBadEmoji(),
+            }, function(err, res) {
+              if (err) {
+                  bot.botkit.log('Failed to add emoji reaction :(', err);
+              }
+            });
+
+            convo.say(pickBaby());
+            getRandomGif('baby', function(imageUrl){
+              convo.say(imageUrl);
+              askHowMany(response, convo);
+              convo.next();
+            });
+          }
+
         }
-    });
-});
-
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
+      },
+      {
+        pattern:  'stop',
+        callback: function(response, convo) {
+          bot.api.reactions.add({
+            timestamp: response.ts,
+            channel: response.channel,
+            name: pickBadEmoji(),
+          }, function(err, res) {
+            if (err) {
+                bot.botkit.log('Failed to add emoji reaction :(', err);
             }
+          });
+
+          convo.say(pickBaby());
+          getRandomGif('baby', function(imageUrl){
+            convo.say(imageUrl);
+            convo.next();
+          });
         }
-        ]);
-    });
+      },
+      {
+        default: true,
+        callback: function(response, convo) {
+          convo.say('Sorry I need a number, stupid!');
+          getRandomGif('stupid', function(imageUrl){
+            convo.say(imageUrl);
+            askHowMany(response, convo);
+            convo.next();
+          });
+        }
+      }
+    ]);
+  }
+
+  workout = function(response, convo) {
+    var movement = pickWorkout();
+    convo.say('Ok, lets do '+movement);
+    ready(response, convo);
+    convo.next();
+  }
+
+  ready = function(response, convo) {
+    convo.ask('Are you ready?', [
+      {
+        pattern: 'yes',
+        callback: function(response, convo) {
+          getRandomGif('strong', function(imageUrl){
+            convo.say(imageUrl);
+            convo.next();
+          });
+        }
+      },
+      {
+        pattern: 'no',
+        callback: function(response, convo) {
+          convo.say('Ok, i will give you 1 minute');
+          readyNow(response, convo);    
+          convo.next();
+        }
+      }
+    ]);
+  }
+
+  readyNow = function(response, convo) {
+    setTimeout(function () {
+      convo.ask('How about now?', [
+        {
+          pattern: 'yes',
+          callback: function(response, convo) {
+            getRandomGif('strong', function(imageUrl){
+              convo.say(imageUrl);
+              convo.next();
+            });
+          }
+        },
+        {
+          pattern: 'no',
+          callback: function(response, convo) {
+            convo.say('Ok, i will give you 1 minute');
+            readyNow(response, convo);    
+            convo.next();
+          }
+        }
+      ]);
+    }, 30000);  
+  }
+
+  bot.startConversation(message, sayHello);
+
 });
 
+controller.hears(['shutdown'], 'direct_message,direct_mention', function(bot, message) {
 
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
+  bot.startConversation(message, function(err, convo) {
 
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-    });
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
+    convo.ask('Are you sure you want me to shutdown?', [
+      {
+        pattern: bot.utterances.yes,
+        callback: function(response, convo) {
+            convo.say('Bye!');
+            convo.next();
+            setTimeout(function() {
+                process.exit();
+            }, 3000);
+        }
+      },
+    {
+      pattern: bot.utterances.no,
+      default: true,
+      callback: function(response, convo) {
+        convo.say('*Phew!*');
+        convo.next();
+      }
     }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
+    ]);
+  });
+});
 
-    uptime = uptime + ' ' + unit;
-    return uptime;
+function getRandomGif(keyword, callback){
+  var url = 'http://api.giphy.com/v1/gifs/search?q='+keyword+'&api_key=dc6zaTOxFJmzC';
+    request.get(url)
+    .end(function(err, res){
+      if(err || !res.ok) {
+        console.log(err)
+        return
+      }
+      image = res.body.data[Math.floor(Math.random()*res.body.data.length)];
+      callback(image.images.fixed_height.url);
+    })
+}
+
+function pickIntro() {
+  var items = Array(
+    'I am here to pump you up!',
+    'I am the workoutanator!',
+    'Strength does not come from sitting!'
+  );
+  return items[Math.floor(Math.random()*items.length)];
+}
+
+function pickBaby() {
+  var items = Array(
+    "c'mon is that all you got? silly baby!",
+    "don't be a baby!",
+    'thats why tou have baby arms!'
+  );
+  return items[Math.floor(Math.random()*items.length)];
+}
+
+function pickBadEmoji() {
+  var items = Array(
+    'rage',
+    'rage1',
+    'rage2',
+    'rage3',
+    'rage4'
+  );
+  return items[Math.floor(Math.random()*items.length)];
+}
+
+function pickGoodEmoji() {
+  var items = Array(
+    'muscle::skin-tone-4',
+    'weight_lifter::skin-tone-4',
+    'apple'
+  );
+  return items[Math.floor(Math.random()*items.length)];
+}
+
+
+function pickWorkout() {
+  var items = Array(
+    'a *wall sit*',
+    'some *push ups*',
+    'a *plank*',
+    'some *jumping jacks*',
+    'some *chair dips*'
+  );
+  return items[Math.floor(Math.random()*items.length)];
 }
